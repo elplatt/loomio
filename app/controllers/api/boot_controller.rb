5a3862a5adbe62dbcaad87928f0cd5b7a40d0ad4
Edward L Platt <ed@elplatt.com>
2020-04-16 16:46:31 -0400

    Add debugging to boot.

--- app/controllers/api/boot_controller.rb
+++ app/controllers/api/boot_controller.rb
@@ -4,6 +4,9 @@ class API::BootController < API::RestfulController
   end
 
   def user
+    puts "boot/user"
+    puts current_user
+    puts serialized_pending_identity
     render json: Boot::User.new(current_user, identity: serialized_pending_identity, flash: flash).payload
   end
 end
