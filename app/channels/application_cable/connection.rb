7ecd3d3cb96df3cb89b34bbd4a0bdf563882335b
Edward L Platt <ed@elplatt.com>
2020-04-17 15:46:02 -0400

    Add debug to connection.

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -3,7 +3,10 @@ module ApplicationCable
     identified_by :current_user
 
     def connect
+      puts "ApplicationCable::connect"
+      puts self.current_user
       self.current_user = user || membership || reject_unauthorized_connection
+      puts self.current_user
     end
 
     private
